20325e841e315da6cb7525c1110bf476482fcdbb
shan10832638 <136928126+shan10832638@users.noreply.github.com>
2023-06-18 05:15:46 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://shan10832638.github.io/10832638/model.json';
+let soundModelURL = 'https://shan10832638.github.io/bird666/model.json';
 
 
 function preload() {
@@ -28,7 +28,7 @@ function setup() {
   createCanvas(320, 240);
   // Start classifying
   // The sound model will continuously listen to the microphone
-
+  ThunkableWebviewerExtension.postMessage("ready");
   classifier.classify(gotResult);
 }
 
@@ -51,5 +51,6 @@ function gotResult(error, results) {
   // The results are in an array ordered by confidence.
   // console.log(results[0]);
   label = results[0].label;
-
+  ThunkableWebviewerExtension.postMessage(label);
+  
 }
